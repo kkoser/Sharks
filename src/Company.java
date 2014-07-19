bb3103a55f82cb9675343549eae2312a2d046e4c
Alex Pogue <alex@epogue.com>
2014-07-19 14:31:45 -0500

    Added Company class

---
+++ src/Company.java
@@ -0,0 +1,39 @@
+
+public class Company {
+	private String name;
+	private String type;
+	private int value;
+	private int investedAmount;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
+	public int getInvestedAmount() {
+		return investedAmount;
+	}
+	
+	public void setInvestedAmount(int investedAmount) {
+		this.investedAmount = investedAmount;
+	}
+}
