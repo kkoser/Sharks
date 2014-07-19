68d6059b8a245bfe99e5a69aa59431a95d830e00
kkoser <kkoser@nd.edu>
2014-07-19 15:07:39 -0500

    Added main method implementation

--- src/Sharks.java
+++ src/Sharks.java
@@ -4,6 +4,12 @@ public class Sharks {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println( "Hellow World!" );
+		
+		Investor steve = new Investor( 100000.00, "Steve" );
+		GlobalModel model = new GlobalModel();
+		while( true ) {
+			steve.updateCompanies( model );
+		}
 	}
 
 }
