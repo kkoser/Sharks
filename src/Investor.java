ef3d08c0d0a1aaf1354a72c9954c2424a9799742
kkoser <kkoser@nd.edu>
2014-07-19 14:34:49 -0500

    Added Inverstor class

---
+++ src/Investor.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Investor {
+	private ArrayList<Company> companies;
+	private int money;
+	
+	public ArrayList<Company> getCompanies() {
+		return companies;
+	}
+	public void setCompanies(ArrayList<Company> companies) {
+		this.companies = companies;
+	}
+	
+	public int getMoney() {
+		return money;
+	}
+	
+	public void setMoney(int money) {
+		this.money = money;
+	}
+	
+	
+}
