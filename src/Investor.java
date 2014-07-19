cc48e212d0855a07a7796ba34f7e059b932b23cd
kkoser <kkoser@nd.edu>
2014-07-19 14:53:07 -0500

    Added Investor constructor

--- src/Investor.java
+++ src/Investor.java
@@ -2,7 +2,20 @@ import java.util.ArrayList;
 
 public class Investor {
 	private ArrayList<Company> companies;
-	private int money;
+	private double money;
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Investor( double initialMoney, String name ) {
+		this.setMoney( initialMoney );
+	}
 	
 	public ArrayList<Company> getCompanies() {
 		return companies;
@@ -11,11 +24,11 @@ public class Investor {
 		this.companies = companies;
 	}
 	
-	public int getMoney() {
+	public double getMoney() {
 		return money;
 	}
 	
-	public void setMoney(int money) {
+	public void setMoney(double money) {
 		this.money = money;
 	}
 	
