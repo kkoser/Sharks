52a81b596fcd8e5ffdfd663a8b3b838024c7b462
Alex Pogue <alex@epogue.com>
2014-07-20 08:20:06 -0500

    Add cool pitches!

--- src/Company.java
+++ src/Company.java
@@ -18,13 +18,23 @@ public class Company {
 		this.age = age;
 		this.ownerName = owner;
 		this.categories = categories;
-		ownerType = (int) (Math.random() * 5);
+		ownerType = (int) (Math.random() * 9);
 	}
 	
 	public String getPitch() {
 		String catString = "";
-		for( String s : getCategories() ) {
-			catString += s + ", ";
+		int numCategories = getCategories().size();
+		for(int i = 0; i < numCategories; i++) {
+			if(i != 0 && numCategories > 2) {
+				catString += ", ";
+			}
+			if(i == numCategories - 1 && numCategories > 1) {
+				if(numCategories == 2) {
+					catString += " ";
+				}
+				catString += "and ";
+			}
+			catString += getCategories().get(i);
 		}
 		
 		switch( this.ownerType ) {
@@ -33,11 +43,21 @@ public class Company {
 		case 1:
 			return "Hi... I am here today to pitch my company " + getName() + ". We do work in the areas of " + catString +". PLease invest in us. Please...";
 		case 2:
-			return "Oh, what's the point. You aren't going to invest in me anyway.";
+			return "My company is awesome! It's called " + getName() + " and we work on " + catString + "! Invest!";
 		case 3:
-			return "Hey there! I'm here today to pitch you the biggest opportunity of your life. My company " + getName() + " is going to be the next big thing in " + catString + "! If you don't invest now, it's gonna cost you a lot more next round!";
+			return "Big data, HTML5, lawlcatz, Angular. Buzz words are all over here at " + getName() + " we're the next big thing in " + catString + "!";
 		case 4:
-			return "Farts";
+			return "Get yo' computing on at " + getName() + "! Yo, yo, we do " + catString + "!";
+		case 5:
+			return "Take care of all your freakin' " + catString + " needs here at " + getName() + "!";
+		case 6:
+			return "Who kicks butt at " + catString + "? " + getName() + " does!";
+		case 7:
+			return "Here at " + getName() + ", we love our users, along with " + catString + "!";
+		case 8:
+			return "At " + getName() + " need your financial help to help fund adventures in " + catString + "!";
+		case 9:
+			return "Don't worry, " + getName() + " bans Facebook and Reddit for all employees! Invest in " + catString + "!";
 		}
 		
 		return "";
